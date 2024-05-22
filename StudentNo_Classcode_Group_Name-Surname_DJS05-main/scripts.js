df29cc70cd7b3b7d23ed9b729a260a9efd0d2412
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-05-22 14:17:06 +0200

    Commented out my original code, got the current state of the counter with the 'getState' method, and subscribed a listener function when the state changes

--- StudentNo_Classcode_Group_Name-Surname_DJS05-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS05-main/scripts.js
@@ -1,40 +1,55 @@
-/* class Counter {
-    value = 1;
+class Counter {
+    // Constructor initializes the counter with an initial state
+    constructor() {
+        this.state = { count: 0 };
+        this.listeners = [];
+    }
+
+    // Get the current state of the counter
+    getState() {
+        return this.state;
+    }
+
+    // Subscribe a listener function to be called whenever the state changes
+    subscribe(listener) {
+        this.listeners.push(listener);
+    }
+}
+
+// Create a new counter instance
+const counter = new Counter();
+
+/* { type: 'ADD' }
+{ type: 'SUBTRACT' }
+{ type: 'RESET' } 
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'ADD':
+      return state + 1;
+    case 'SUBTRACT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+let store = Redux.createStore(counter);
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch({ type: 'ADD' }); 
 
     add() {
-        this.value += 1
+        this.state.value++;
     }
 
     subtract() {
-        this.value -= 1
+        this.state.value--;
     }
 
     reset() {
-        this.value.reset()
+        this.state.value = 0;
     }
-} */
 
-const state = {
-    value: 0,
-}
-
-const add = (state) => {
-    return {
-        ...state,
-        value: state.value + 1
-    }
-}
-
-const subtract = (state) => {
-    return {
-        ...state,
-        value: state.value - 1
-    }
-}
-
-const reset = (state) => {
-    return {
-        ...state,
-        value: state.value.reset()
-    }
-}
+ */
