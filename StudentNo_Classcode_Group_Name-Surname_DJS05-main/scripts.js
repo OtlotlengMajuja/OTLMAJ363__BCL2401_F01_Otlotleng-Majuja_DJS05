5fd767c7fdab98eaa457f15c0381ab27239aeb80
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-05-22 14:57:49 +0200

    Dispatched my actions and named them correctly

--- StudentNo_Classcode_Group_Name-Surname_DJS05-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS05-main/scripts.js
@@ -18,13 +18,13 @@ class Counter {
     // Dispatch action to the counter, which will update the state based on the action type
     dispatch(action) {
         // If the action type is 'add', increment the count in the state
-        if (action.type === 'add') {
+        if (action.type === 'ADD') {
             this.state = { count: this.state.count + 1 };
             // If the action type is 'subtract', decrement the count in the state
-        } else if (action.type === 'subtract') {
+        } else if (action.type === 'SUBTRACT') {
             this.state = { count: this.state.count - 1 };
             // If the action type is 'reset', set the count to 0
-        } else if (action.type === 'reset') {
+        } else if (action.type === 'RESET') {
             this.state = { count: 0 };
         }
 
@@ -35,6 +35,27 @@ class Counter {
 
 // Create a new counter instance
 const counter = new Counter();
+
+// Subscribe a listener to the counter that will log the state whenever it changes
+counter.subscribe(() => console.log(store.getState()));
+
+// Dispatch an 'add' action to the counter, which will increment the count to 1
+counter.dispatch({ type: 'ADD' }); // { count: 1 }
+
+// Dispatch another 'add' action to the counter, which will increment the count to 2
+counter.dispatch({ type: 'ADD' }); // { count: 2 }
+
+// Dispatch a 'subtract' action to the counter, which will decrement the count to 1
+counter.dispatch({ type: 'SUBTRACT' }); // { count: 1 }
+
+// Dispatch a 'reset' action to the counter, which will set the count to 0
+counter.dispatch({ type: 'RESET' }); // { count: 0 }
+
+
+
+
+
+
 
 /* { type: 'ADD' }
 { type: 'SUBTRACT' }
