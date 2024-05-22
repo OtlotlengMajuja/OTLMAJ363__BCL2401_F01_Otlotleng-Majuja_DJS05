7c4e414f83dae71ef17c67aa1c0ae431c221b812
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-05-22 10:51:30 +0200

    Included boilerplate HTML as well as two additional JavaScript files

--- StudentNo_Classcode_Group_Name-Surname_DJS05-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS05-main/scripts.js
@@ -1,15 +1,40 @@
-class Counter {
+/* class Counter {
     value = 1;
 
-    increase() {
+    add() {
         this.value += 1
     }
 
-    decrease() {
+    subtract() {
         this.value -= 1
     }
 
     reset() {
         this.value.reset()
+    }
+} */
+
+const state = {
+    value: 0,
+}
+
+const add = (state) => {
+    return {
+        ...state,
+        value: state.value + 1
+    }
+}
+
+const subtract = (state) => {
+    return {
+        ...state,
+        value: state.value - 1
+    }
+}
+
+const reset = (state) => {
+    return {
+        ...state,
+        value: state.value.reset()
     }
 }
